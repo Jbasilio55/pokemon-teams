8ee5389d6eb23082e1517b7b8ae15457cd869ee4
Jorge Basilio <J.l.Basilio55@gmail.com>
2020-03-18 10:53:16 -0400

    fourth commit

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -3,13 +3,16 @@ const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 
 const main = document.getElementsByTagName('main')[0]
+const release = document.getElementsByClassName('release')[0]
 console.log(main)
+console.log(release)
 
 
 fetch(TRAINERS_URL)
 .then(response => response.json())
 .then (trainers => {
     trainers.forEach(function(trainer){
+        console.log(trainer.pokemons)
         main.innerHTML+=`
         <div class="card" data-id="${trainer.id}"><p>${trainer.name}</p>
         <button data-trainer-id="${trainer.id}">Add Pokemon</button>
@@ -17,16 +20,33 @@ fetch(TRAINERS_URL)
         <li>${trainer.pokemons.nickname} <button class="release" data-pokemon-id=${trainer.pokemons.id}>Release</button></li>
         </ul>
         </div>
-        `
-        
+        `,
+        trainer.pokemons.forEach(function(pokemon){
+            console.log(trainer.pokemons.id)
+            console.log(trainer.pokemons.nickname)
+        })
     })
 })
 
 main.addEventListener("click", function(event){
+    console.log(event.target)
     if(event.target.innerText === "Add Pokemon"){
-        
+        fetch(POKEMONS_URL, {
+            method: "post",
+            headers: {
+                "content-type": "application/json",
+                "accept": "application/json"
+            },
+            body:JSON.stringify(pokemon)
+        })
+        .then(response => response.json())
+        .then(pokemon => {
+
+        })
     }
 })
+
+
 
 
 {/* <li>Jacey (Kakuna) <button class="release" data-pokemon-id="140">Release</button></li>
