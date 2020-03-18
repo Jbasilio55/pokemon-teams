75cdf161e216d7d60549c85d5ba30729f06d3343
Jorge Basilio <J.l.Basilio55@gmail.com>
2020-03-18 09:51:48 -0400

    first commit

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,12 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+fetch(BASE_URL)
+.then(response => response.jason())
+.then (pokemons => {
+    pokemons.forEach(function(pokemon){
+        
+    })
+})
+
