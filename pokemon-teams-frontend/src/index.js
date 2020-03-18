80df6bd18bde5215871642bd21bcdf5ba3e7bc2d
Jorge Basilio <J.l.Basilio55@gmail.com>
2020-03-18 10:32:54 -0400

    third commit

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -3,26 +3,31 @@ const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 
 const main = document.getElementsByTagName('main')[0]
+console.log(main)
 
 
 fetch(TRAINERS_URL)
-.then(response => response.jason())
+.then(response => response.json())
 .then (trainers => {
     trainers.forEach(function(trainer){
         main.innerHTML+=`
-        <div class="card" data-id="1"><p>${trainer.name}</p>
+        <div class="card" data-id="${trainer.id}"><p>${trainer.name}</p>
         <button data-trainer-id="${trainer.id}">Add Pokemon</button>
         <ul>
-        <li>${trainer.pokemons.nickname}<button class="release" data-pokemon-id="${trainer.pokemons.id}">Release</button></li>
-        <li>Zachariah (Ditto) <button class="release" data-pokemon-id="141">Release</button></li>
-        <li>Mittie (Farfetch'd) <button class="release" data-pokemon-id="149">Release</button></li>
-        <li>Rosetta (Eevee) <button class="release" data-pokemon-id="150">Release</button></li>
-        <li>Rod (Beedrill) <button class="release" data-pokemon-id="151">Release</button></li>
+        <li>${trainer.pokemons.nickname} <button class="release" data-pokemon-id=${trainer.pokemons.id}>Release</button></li>
         </ul>
         </div>
         `
+        
     })
 })
+
+main.addEventListener("click", function(event){
+    if(event.target.innerText === "Add Pokemon"){
+        
+    }
+})
+
 
 {/* <li>Jacey (Kakuna) <button class="release" data-pokemon-id="140">Release</button></li>
 <li>Zachariah (Ditto) <button class="release" data-pokemon-id="141">Release</button></li>
